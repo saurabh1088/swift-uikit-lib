f32a4b67a5dd23e331d2b480f61295d736a6e813
Saurabh Verma <saurabh1088@gmail.com>
2025-02-06 08:24:52 +0530

    Started writing test cases using Swift's Testing framework.

--- Tests/SwiftUIKitLibTests/SwiftUIKitLibTests.swift
+++ Tests/SwiftUIKitLibTests/SwiftUIKitLibTests.swift
@@ -1,6 +1,29 @@
 import Testing
+import UIKit
 @testable import SwiftUIKitLib
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite("Single Detail Table View Cell Tests")
+class SingleDetailTableViewCellTests {
+    
+    var tableView: UITableView!
+    var cell: SingleDetailTableViewCell!
+    
+    func setup() async throws {
+        tableView = await UITableView()
+        await tableView.register(UINib(nibName: "SingleDetailTableViewCell", bundle: nil), forCellReuseIdentifier: "SingleDetailTableViewCell")
+        cell = await tableView.dequeueReusableCell(withIdentifier: "SingleDetailTableViewCell") as? SingleDetailTableViewCell
+    }
+    
+    
+    @Test func testDetailLabelSetText() async throws {
+        // Given
+        let testText = "Test Text"
+        try await setup()
+                
+        // When
+        await cell.setDetailText(testText)
+        
+        // Then
+        await #expect(cell.detailLabel.text == testText, "The text set should match the test text")
+    }
 }
