c3ad4d51e1e6468c76dedf4b9358db1a4dc686e6
Saurabh Verma <saurabh1088@gmail.com>
2025-02-12 18:53:07 +0530

    Added Bundle extension to access from importing projects.

---
+++ Sources/SwiftUIKitLib/Bundle+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  SwiftUIKitLib
+//
+//  Created by Saurabh Verma on 12/02/25.
+//
+
+import Foundation
+
+public extension Bundle {
+    static var packageBundleSwiftUIKitLib: Bundle {
+        return Bundle.module
+    }
+}
