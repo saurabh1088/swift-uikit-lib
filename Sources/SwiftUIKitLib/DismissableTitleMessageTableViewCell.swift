3344c5b4071975235a1440238ac1850c19650090
Saurabh Verma <saurabh1088@gmail.com>
2025-02-17 19:39:45 +0530

    Added cell DismissableTitleMessageTableViewCell.

---
+++ Sources/SwiftUIKitLib/DismissableTitleMessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DismissableTitleMessageTableViewCell.swift
+//  SwiftUIKitLib
+//
+//  Created by Saurabh Verma on 17/02/25.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+class DismissableTitleMessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
+
+#endif
