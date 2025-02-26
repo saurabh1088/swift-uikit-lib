a3cba1cf2e0162156a26a684f0d87f4e6661cd69
Saurabh Verma <saurabh1088@gmail.com>
2025-02-26 22:54:26 +0530

    Completed unit tests for DismissableTitleMessageTableViewCell.

--- Tests/SwiftUIKitLibTests/DismissableTitleMessageTableViewCellTests.swift
+++ Tests/SwiftUIKitLibTests/DismissableTitleMessageTableViewCellTests.swift
@@ -54,5 +54,117 @@ class DismissableTitleMessageTableViewCellTests {
         // Then
         #expect(identifier == expectedIdentifier, "Reuse identifier should match the class name")
     }
+    
+    @MainActor @Test func testConfigureSetsTitle() throws {
+        // Given
+        try setup()
+        let title = "Test Title"
+        
+        // When
+        sut.configure(with: title, message: "", icon: nil)
+        
+        // Then
+        #expect(sut.titleLabel.text == title, "Title label should be set to the provided title")
+    }
+    
+    @MainActor @Test func testConfigureSetsMessage() throws {
+        // Given
+        try setup()
+        let message = "Test Message"
+        
+        // When
+        sut.configure(with: "", message: message, icon: nil)
+        
+        // Then
+        #expect(sut.messageLabel.text == message, "Message label should be set to the provided message")
+    }
+    
+    @MainActor @Test func testConfigureSetsIcon() throws {
+        // Given
+        try setup()
+        let icon = UIImage(systemName: "star")
+        
+        // When
+        sut.configure(with: "", message: "", icon: icon)
+        
+        // Then
+        #expect(sut.leadingIconImageView.image == icon, "Icon image view should be set to the provided image")
+    }
+    
+    @MainActor @Test func testConfigureSetsDismissAction() throws {
+        // Given
+        try setup()
+        var dismissCalled = false
+        let dismissAction: () -> Void = { dismissCalled = true }
+        
+        // When
+        sut.configure(with: "", message: "", icon: nil, dismissAction: dismissAction)
+        sut.dismissButtonTapped(UIButton())
+        
+        // Then
+        #expect(dismissCalled == true, "Dismiss action should be executed when dismiss button is tapped")
+    }
+        
+    @MainActor @Test func testConfigureSetsActionButtonAction() throws {
+        // Given
+        try setup()
+        var actionCalled = false
+        let actionButtonAction: () -> Void = { actionCalled = true }
+        
+        // When
+        sut.configure(with: "", message: "", icon: nil, actionButtonAction: actionButtonAction)
+        sut.actionButtonTapped(UIButton())
+        
+        // Then
+        #expect(actionCalled == true, "Action button action should be executed when action button is tapped")
+    }
+        
+    @MainActor @Test func testDismissButtonControlReturnsDismissButton() throws {
+        // Given
+        try setup()
+        let dismissButton = sut.dismissButton
+        
+        // When
+        let control = sut.dismissButtonControl
+        
+        // Then
+        #expect(control === dismissButton, "dismissButtonControl should return the same instance as dismissButton")
+    }
+        
+    @MainActor @Test func testActionButtonControlReturnsActionButton() throws {
+        // Given
+        try setup()
+        let actionButton = sut.actionButton
+        
+        // When
+        let control = sut.actionButtonControl
+        
+        // Then
+        #expect(control === actionButton, "actionButtonControl should return the same instance as actionButton")
+    }
+        
+    @MainActor @Test func testDismissButtonTappedDoesNothingWhenNoActionSet() throws {
+        // Given
+        try setup()
+        sut.dismissAction = nil
+        
+        // When
+        sut.dismissButtonTapped(UIButton()) // Should not crash
+        
+        // Then
+        #expect(sut.dismissAction == nil, "No action should be triggered when dismissAction is nil")
+    }
+        
+    @MainActor @Test func testActionButtonTappedDoesNothingWhenNoActionSet() throws {
+        // Given
+        try setup()
+        sut.actionButtonAction = nil
+        
+        // When
+        sut.actionButtonTapped(UIButton()) // Should not crash
+        
+        // Then
+        #expect(sut.actionButtonAction == nil, "No action should be triggered when actionButtonAction is nil")
+    }
 }
 
