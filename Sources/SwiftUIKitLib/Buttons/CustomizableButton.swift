716c4886d1dc9f94a1fd10123e49e4d8668f676f
Saurabh Verma <saurabh1088@gmail.com>
2025-03-04 08:21:16 +0530

    Added folder group for custom buttons and added skeleton class for implementing a customizable button.

---
+++ Sources/SwiftUIKitLib/Buttons/CustomizableButton.swift
@@ -0,0 +1,27 @@
+//
+//  CustomizableButton.swift
+//  SwiftUIKitLib
+//
+//  Created by Saurabh Verma on 04/03/25.
+//
+
+import UIKit
+
+/**
+ A highly customizable button built on top of UIButton, supporting various styles, states, and interactions.
+ */
+class CustomizableButton: UIButton {
+    
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
